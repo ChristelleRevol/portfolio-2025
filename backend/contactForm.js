862803564fdcaa7e62aa0d9607abe1e341fe8be3
root <root@DESKTOP-H2ORS4T.localdomain>
2025-05-28 13:29:44 +0200

    retakes 8 deploy

--- backend/contactForm.js
+++ backend/contactForm.js
@@ -51,14 +51,3 @@ app.post("/send", async (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Serveur en écoute sur le port ${PORT}`);
 });
-
-// const path = require("path");
-
-// // Sert les fichiers React si on est en production
-// // if (process.env.NODE_ENV === "production") {
-// // 	app.use(express.static(path.join(__dirname, "..frontend/build")));
-
-// // 	app.get("*", (req, res) => {
-// // 		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-// // 	});
-// // }
